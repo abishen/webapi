ed48a886a7ebfc8bf905cddfb3f29e75aa5b8c8b
Dillon Dhayanandan <abishens@gmail.com>
2026-02-06 20:06:19 +0000

    modifed the endpoint get by id

--- webapi/handlers.go
+++ webapi/handlers.go
@@ -10,6 +10,18 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func RegisterRoutes(router *gin.Engine) {
 	router.GET("/album", getAlbums)
+	router.GET("/album/:id", getAlbumByID)
 }
